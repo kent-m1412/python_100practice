114e19cee8185ba1e382a4ad27ed015548630eb2
Kento Masuda <masuda.kento.q3@dc.tohoku.ac.jp>
2026-04-17 20:38:23 +0900

    feat: add problems_1to5.py

---
+++ python100practice/problems_1to5.py
@@ -0,0 +1,22 @@
+#problem1
+print("Hello World")
+
+#problem2
+print(1)
+print(2)
+print(3)
+
+#problem3
+print(123,"Hello")
+
+#problem4
+print(1)
+#print(2)
+print(3)
+
+#problem5
+""""
+print(1)
+print(2)
+print(3)
+"""

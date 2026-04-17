fd91ff29e8988f1d6e8fb76ee40bf8c28a4bc729
Kento Masuda <masuda.kento.q3@dc.tohoku.ac.jp>
2026-04-17 20:38:38 +0900

    feat: add problems_6to17.py

---
+++ python100practice/problems_6to17.py
@@ -0,0 +1,50 @@
+#problem6
+x = 10
+print(x)
+
+#problem7
+x = 1 + 2
+print(x)
+
+#problem8
+x = str("1") + str("2")
+print(x)
+
+#problem9
+x = 10
+print(str(x))
+x = '10'
+print(int(x))
+
+#problem10
+print("1\n2\n3")
+
+#problem11
+print("C:\\test")
+
+#problem12
+print(r"C:\test")
+
+#problem13
+name = "Python"
+print(f"Hello {name}")
+
+#problem14
+print(len(name))
+
+#problem15
+print(name[0:2])
+
+#problem16
+x = None
+print(x)
+
+#problem
+a = 10
+b = 3.14
+c = "Hello, Python!"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))

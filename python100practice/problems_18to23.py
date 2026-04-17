293dd2badeed1a8e9be35548b6cd9e1565f002c9
Kento Masuda <masuda.kento.q3@dc.tohoku.ac.jp>
2026-04-17 20:38:50 +0900

    feat: add problems_18to23.py

---
+++ python100practice/problems_18to23.py
@@ -0,0 +1,26 @@
+#problem18
+lst = [1,2,3]
+print(lst)
+print(lst[0])
+
+#problem19
+lst = [1,2,3]
+print(lst[0:2])
+
+#problem20
+lst = [1,2,3]
+lst = lst + [4]
+print(lst)
+
+#problem21
+lst = [1,2,3]
+print(len(lst))
+
+#problem22
+lst = [1,2,3]
+lst.remove(2)
+print(lst)
+
+#problem23
+lst = [1,2] + [3,4]
+print(lst)

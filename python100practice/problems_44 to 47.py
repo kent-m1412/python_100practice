0ac966f5c8880ae04dc5b82f878720334f9f1848
Kento Masuda <masuda.kento.q3@dc.tohoku.ac.jp>
2026-04-17 20:39:32 +0900

    feat: add problems_44to47.py

---
+++ python100practice/problems_44 to 47.py
@@ -0,0 +1,23 @@
+#problem44
+x = "1,2,3"
+x.replace(",","&")
+print(x)
+
+#problem45
+x = "1,2,3"
+lst = x.split(",")
+print(lst)
+y = '&'.join(lst)
+print(y)
+
+#problem46
+x = 10
+y = 3
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+
+#problem47
+lst = [1,2,3]
+print(max(lst))
